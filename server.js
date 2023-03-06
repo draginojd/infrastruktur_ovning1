d607ed1e4f47f6a417488b90d9c79e8b2933b035
draginojd <arminfazlikhan@gmail.com>
2023-03-06 13:54:45 +0000

    New files

---
+++ server.js
@@ -0,0 +1,18 @@
+
+const express = require("express");
+
+const path = require("path");
+
+const app = express();
+
+const port = 5100;
+
+app.use(express.static("dist"));
+
+app.get('*', (req, res) => {
+
+    res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+
+app.listen(port, () => console.log('Listening on port ' + port));
